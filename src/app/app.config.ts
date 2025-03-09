dad7e9e5c7945527fd520e772f15913722a104a1
Alexander Hörst <hoerst.alexander@gmail.com>
2025-03-09 14:54:39 +0100

    add firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,8 @@ import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -21,5 +23,5 @@ export const appConfig: ApplicationConfig = {
       },
       defaultLanguage: 'de'
     })
-  )]
+  ), provideFirebaseApp(() => initializeApp({"projectId":"portfolio-14d2c","appId":"1:966344824986:web:f4bb2b5ab4cef7970fa723","storageBucket":"portfolio-14d2c.firebasestorage.app","apiKey":"AIzaSyD0kDUm2C3D3ox3sYmfigDsvf7fITIvAeE","authDomain":"portfolio-14d2c.firebaseapp.com","messagingSenderId":"966344824986"})), provideFirestore(() => getFirestore())]
 };
